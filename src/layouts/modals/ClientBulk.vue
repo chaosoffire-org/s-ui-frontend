b971d39d84e36ae0266b0efc2b6471222ff2405d
Chaosoffire <81634128+chaosoffire@users.noreply.github.com>
2025-11-30 03:14:08 +0800

    fix: bulk create add uuid

--- src/layouts/modals/ClientBulk.vue
+++ src/layouts/modals/ClientBulk.vue
@@ -9,39 +9,32 @@
         <v-container style="padding: 0;">
           <v-row>
             <v-col cols="12" sm="6" md="4">
-              <v-text-field v-model.number="count" type="number" min="1" max="100" :label="$t('count')" hide-details></v-text-field>
+              <v-text-field v-model.number="count" type="number" min="1" max="100" :label="$t('count')"
+                hide-details></v-text-field>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="12" sm="8">
-              <v-combobox
-                chips
-                multiple
-                v-model="bulkData.name"
-                :items="patterns"
-                :label="$t('client.name')"
+              <v-combobox chips multiple v-model="bulkData.name" :items="patterns" :label="$t('client.name')"
                 hide-details>
               </v-combobox>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="12" sm="8">
-              <v-combobox
-                chips
-                multiple
-                v-model="bulkData.desc"
-                :items="patterns"
-                :label="$t('client.desc')"
+              <v-combobox chips multiple v-model="bulkData.desc" :items="patterns" :label="$t('client.desc')"
                 hide-details>
               </v-combobox>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="12" sm="6" md="4">
-              <v-combobox v-model="bulkData.group" :items="groups" :label="$t('client.group')" hide-details></v-combobox>
+              <v-combobox v-model="bulkData.group" :items="groups" :label="$t('client.group')"
+                hide-details></v-combobox>
             </v-col>
             <v-col cols="12" sm="6" md="4">
-              <v-text-field v-model.number="bulkData.Volume" type="number" min="0" :label="$t('stats.volume')" suffix="GiB" hide-details></v-text-field>
+              <v-text-field v-model.number="bulkData.Volume" type="number" min="0" :label="$t('stats.volume')"
+                suffix="GiB" hide-details></v-text-field>
             </v-col>
             <v-col cols="12" sm="6" md="4">
               <DatePick :expiry="bulkData.expiry" @submit="setDate" />
@@ -49,34 +42,18 @@
           </v-row>
           <v-row>
             <v-col>
-              <v-combobox
-                v-model="bulkData.clientInbounds"
-                :items="inboundTags"
-                :label="$t('client.inboundTags')"
-                :return-object="false"
-                multiple
-                chips
-                hide-details
-              ></v-combobox>
+              <v-combobox v-model="bulkData.clientInbounds" :items="inboundTags" :label="$t('client.inboundTags')"
+                :return-object="false" multiple chips hide-details></v-combobox>
             </v-col>
           </v-row>
         </v-container>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn
-          color="primary"
-          variant="outlined"
-          @click="closeModal"
-        >
+        <v-btn color="primary" variant="outlined" @click="closeModal">
           {{ $t('actions.close') }}
         </v-btn>
-        <v-btn
-          color="primary"
-          variant="tonal"
-          :loading="loading"
-          @click="saveChanges"
-        >
+        <v-btn color="primary" variant="tonal" :loading="loading" @click="saveChanges">
           {{ $t('actions.save') }}
         </v-btn>
       </v-card-actions>
@@ -117,28 +94,28 @@ export default {
   methods: {
     resetData() {
       this.count = 1,
-      this.clients = [],
-      this.bulkData = {
-        name: [this.patterns[1], "-", this.patterns[0]],
-        desc: [],
-        group: '',
-        clientInbounds: [],
-        expiry: 0,
-        Volume: 0,
-      }
+        this.clients = [],
+        this.bulkData = {
+          name: [this.patterns[1], "-", this.patterns[0]],
+          desc: [],
+          group: '',
+          clientInbounds: [],
+          expiry: 0,
+          Volume: 0,
+        }
     },
     closeModal() {
       this.$emit('close')
     },
     async saveChanges() {
       if (!this.$props.visible) return
-      if (this.bulkData.name.findIndex(n => typeof(n) == 'object') == -1) {
+      if (this.bulkData.name.findIndex(n => typeof (n) == 'object') == -1) {
         push.error(i18n.global.t('error.dplData'))
         return
       }
       this.clients = []
       this.loading = true
-      for(let i=0;i<this.count;i++){
+      for (let i = 0; i < this.count; i++) {
         const name = this.genByPattern(this.bulkData.name, i)
         this.clients.push(createClient({
           enable: true,
@@ -146,7 +123,7 @@ export default {
           config: randomConfigs(name),
           inbounds: this.bulkData.clientInbounds,
           links: [],
-          volume: this.bulkData.Volume*(1024 ** 3),
+          volume: this.bulkData.Volume * (1024 ** 3),
           expiry: this.bulkData.expiry,
           up: 0,
           down: 0,
@@ -161,18 +138,20 @@ export default {
       if (success) this.closeModal()
       this.loading = false
     },
-    genByPattern(pattern: any[], order :number){
+    genByPattern(pattern: any[], order: number) {
       if (pattern.length == 0) return RandomUtil.randomSeq(8)
       let result = ''
       pattern.forEach(p => {
-        switch(typeof p){
+        switch (typeof p) {
           case 'object':
-            switch(p.value){
+            switch (p.value) {
               case "random":
                 result += RandomUtil.randomSeq(8)
+                result += "-"
+                result += RandomUtil.randomUUID()
                 break
               case "order":
-                result += order+1
+                result += order + 1
             }
             break
           default:
@@ -181,7 +160,7 @@ export default {
       })
       return result
     },
-    setDate(v:number){
+    setDate(v: number) {
       this.bulkData.expiry = v
     }
   },
